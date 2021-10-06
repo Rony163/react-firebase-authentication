d0c8c61d86272c228c494bc32135599363d9dd62
Rony163 <minhajulislamrony43@gmail.com>
2021-10-07 02:51:55 +0600

    google login done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import initializeAuthentication from './Firebase/firebase.init';
+import { useState } from 'react';
+
+initializeAuthentication();
+const googleProvider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const googleUser = result.user;
+        const { displayName, email, photoURL } = googleUser;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(loggedInUser);
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        user.name && <div>
+          <h2>Name: {user.name}</h2>
+          <p>Email:{user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAklC0HHWR9CA9iT4rvw4sb2dT5MD_vbMw",
+    authDomain: "react-firebase-authentic-2a2ef.firebaseapp.com",
+    projectId: "react-firebase-authentic-2a2ef",
+    storageBucket: "react-firebase-authentic-2a2ef.appspot.com",
+    messagingSenderId: "169443358824",
+    appId: "1:169443358824:web:d5b855f7b33328998fe3dd"
+};
+
+export default firebaseConfig;
